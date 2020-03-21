6be83c517fed8989c6fae7bfdccb632bf02f4e89
Amber Chunn <a.77@me.com>
2020-03-20 17:04:24 -0700

    Added Nav to Header Component & Initial Router Set-up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import Header from "./components/Header.js";
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Header from './components/Header.js';
 
-export default function App() {
-  return (
-    <main data-testid='app'>
-      <Header />
-    </main>
-  );
+import CharacterList from './components/CharacterList.js';
+import WelcomePage from './components/WelcomePage.js';
+
+import styled from 'styled-components';
+
+export default function App(props) {
+	return (
+		<main data-testid="app">
+			<Header />
+			<Route exact path="/components/WelcomePage">
+				<WelcomePage />
+			</Route>
+			<Route path="/components/CharacterList">
+				<CharacterList />
+			</Route>
+			{/* {console.log(character)} */}
+		</main>
+	);
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+	// TODO: Add useState to track data from useEffect
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+	const [character, setcharacter] = useState([]);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+	useEffect(() => {
+		// const id = props.match.params.id;
+		axios
+			.get(
+				`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`
+			)
+			.then(response => {
+				setcharacter(response.data);
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}, []);
+
+	return (
+		<section className="character-list">
+			{console.log(character)}
+			<h2>TODO: `array.map()` over your state here!</h2>
+			{character.map(toon => {
+				return toon.results;
+			})}
+		</section>
+	);
 }
